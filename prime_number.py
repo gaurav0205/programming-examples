ec904e45562956ef87d0083fb5ee5949e75c45ae
gaurav <gaurav.mandal455@gmail.com>
2017-10-29 19:33:10 +0530

    Updated the prime number script

--- prime_number.py
+++ prime_number.py
@@ -5,11 +5,12 @@ num = int(input("Enter a number: "))
 # prime numbers are greater than 1
 if num > 1:
    for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           break
-   else:
-       print(num,"is a prime number")
-       
+      if (num % i) == 0:
+         print(num,"is not a prime number")
+         break
+      else:
+          print(num,"is a prime number")
+          break
 else:
-   print(num,"is not a prime number")
+   print("Please enter a number greater than 1")
+   
